739d7eab1a336d398777eec672a5cdaacd9ad042
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2018-11-24 21:45:11 +0100

    ---------- Shopping Drive ----------
    - register layouts are done

--- app/app/src/main/java/com/example/j_lds/shoppingdrive/Login.java
+++ app/app/src/main/java/com/example/j_lds/shoppingdrive/Login.java
@@ -7,27 +7,39 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
 
-    private Button register_button;
-    private Button login_button;
-    private Button pwd_button;
-    private Button save;
+    Button register_button;
+    Button login_button;
+    Button pwd_button;
+
+    Button back_to_login;
+    Button next;
+
+    Button back_to_regist1;
+    Button save;
 
     private EditText user_et;
     private EditText user_pwd_et;
-    private EditText userName_regis_et;
-    private EditText user_pwd_regis_et;
+    private EditText firstName_regis_et;
+    private EditText lastName_regis_et;
     private EditText email_et;
     private EditText phoneNumber_et;
-    private EditText address_et;
-    private EditText zipCode_et;
+    private EditText user_regist_pwd_et;
+    private EditText user_regist_cpwd_et;
 
-    private String user, pwd, userName, user_pwd, email, phoneNumber, address, zipCode;
+    private EditText streetAddress_et;
+    private EditText cityAddress_et;
+    private EditText countryCodeAddress_et;
+    private EditText countryAddress_et;
+
+    private String user_st, pwd_st, firstname, lastname, email, phoneNumber, pwd, confirm_pwd, address, streetAddress, cityAddress, countryCodeAddress, countryAddress;
+
 
     private FirebaseDatabase database;
     private DatabaseReference myRef;
@@ -38,67 +50,119 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference();
-//        userDbReference = myRef.child("user");
-//        articleDbReference = myRef.child("article");
-//        userFirstnameDbReference = userDbReference.child("0").child("firstname");
 
         //Hides MACC bar at the top.................................................................
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        //Database..................................................................................
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+//        userDbReference = myRef.child("user");
+//        articleDbReference = myRef.child("article");
+//        userFirstnameDbReference = userDbReference.child("0").child("firstname");
+
+
+        Login_layout();
+    }
+
+    private void Login_layout(){
+        setContentView(R.layout.activity_login);
+
         //declaration on login side.................................................................
         user_et = findViewById(R.id.editText_user);
-        user_pwd_et = findViewById(R.id.editText_pwd);
+        user_pwd_et = findViewById(R.id.editText_user_pwd);
+
         login_button = findViewById(R.id.button_login);
         pwd_button = findViewById(R.id.button_pwd);
         register_button = findViewById(R.id.button_register);
 
-        //declaration on register side..............................................................
-        userName_regis_et = findViewById(R.id.editText_userName);
-        user_pwd_regis_et = findViewById(R.id.editText_user_pwd);
-        email_et = findViewById(R.id.editText_email);
-        phoneNumber_et = findViewById(R.id.editText_phoneNumber);
-        address_et = findViewById(R.id.editText_address);
-        zipCode_et = findViewById(R.id.editText_zipCode);
-
-        save = findViewById(R.id.button_save);
-
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                FirebaseDatabase database = FirebaseDatabase.getInstance();
+//                DatabaseReference myRef = database.getReference("message");
+//
+//                myRef.setValue("Hello, World!");
+                user_st = user_et.getText().toString().trim();
+                pwd_st = user_pwd_et.getText().toString().trim();
+                Toast.makeText(getBaseContext(), "Login in",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                register_layout();
-            }
-        });
-
-        login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("message");
-
-                myRef.setValue("Hello, World!");
+                Register1_layout();
             }
         });
     }
 
-    private void register_layout(){
-        setContentView(R.layout.layout_register);
+    private void Register1_layout(){
+        setContentView(R.layout.layout_register_p1);
+
+        //declaration on register side..............................................................
+        firstName_regis_et = findViewById(R.id.editText_firstName);
+        lastName_regis_et = findViewById(R.id.editText_lastName);
+        email_et = findViewById(R.id.editText_email);
+        phoneNumber_et = findViewById(R.id.editText_phoneNumber);
+        user_regist_pwd_et = findViewById(R.id.editText_user_regist_pwd);
+        user_regist_cpwd_et = findViewById(R.id.editText_confirm_user_pwd);
+
+        back_to_login = findViewById(R.id.button_backToLogin);
+        next = findViewById(R.id.button_next);
+
+        back_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login_layout();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstname = firstName_regis_et.getText().toString();
+                lastname = lastName_regis_et.getText().toString();
+                email = email_et.getText().toString();
+                phoneNumber = phoneNumber_et.getText().toString();
+                pwd = user_regist_pwd_et.getText().toString();
+                confirm_pwd = user_regist_cpwd_et.getText().toString();
+                Register2_layout();
+            }
+        });
+
+    }
+    private void Register2_layout(){
+        setContentView(R.layout.layout_register_p2);
+
+        streetAddress_et = findViewById(R.id.editText_street_address);
+        cityAddress_et = findViewById(R.id.editText_city_address);
+        countryCodeAddress_et = findViewById(R.id.editText_country_code_address);
+        countryAddress_et = findViewById(R.id.editText_country_address);
+
+        back_to_regist1 = findViewById(R.id.button_back_regist1);
+        save = findViewById(R.id.button_save);
+
+        back_to_regist1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register1_layout();
+            }
+        });
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Register();
+                streetAddress = streetAddress_et.getText().toString().trim();
+                cityAddress = cityAddress_et.getText().toString().trim();
+                countryCodeAddress = countryCodeAddress_et.getText().toString().trim();
+                countryAddress = countryAddress_et.getText().toString().trim();
+                address = streetAddress+" "+cityAddress+" "+countryCodeAddress+" "+countryAddress;
+
+                Toast.makeText(getBaseContext(), "saved!!! ",Toast.LENGTH_SHORT).show();
             }
         });
-        myRef.setValue("Hello, World!");
-    }
-
-    private void Register(){
-
     }
 }
